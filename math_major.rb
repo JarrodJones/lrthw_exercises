f8b59a2f721191f147681749ca5fb412b5ce5e28
Jarrod Jones <Jaye1200@icloud.com>
2016-02-29 14:19:41 -0700

    Reformatted to appease Rubocop
    [113796051]

--- math_major.rb
+++ math_major.rb
@@ -5,10 +5,12 @@ gets.chomp
 
 # We will make ruby do the math to solve the word problem.
 
-puts "If Jenny has 3 cookies, Jake has 7 cookies, and Lilly has 5 cookies and they all decide to eat two of their cookies
-how many do they each have left?"
+puts "If Jenny has 3 cookies, Jake has 7 cookies,
+and Lilly has 5 cookies and they all decide to eat
+two of their cookies how many do they each have left?"
 
-puts "Jenny eats 2 so she has #{3 - 2} , Jake eats 2 so he has #{7 - 2}, and Lilly eats 2 so she has #{5 - 2}"
+puts "Jenny eats 2 so she has #{3 - 2} , Jake eats 2 so he has #{7 - 2},
+and Lilly eats 2 so she has #{5 - 2}"
 
 puts "Does Jenny have more cookies than Jake? #{3 - 2 > 7 - 2}"
 
