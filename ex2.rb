a15c878ac8d6b06c45eb10e1e1ba02b1ff23fe51
Jarrod Jones <Jaye1200@icloud.com>
2016-02-29 13:02:47 -0700

    Created the program for lrthw chapter 2
    [13796047]

---
+++ ex2.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+#In this chapter we learn that by using a pound sign you can make a comment
+
+puts "I could have code like this." # Comments help by allowing the writer to use plain english to say what a section of code does
+
+
+#Which means that this code will not run but:
+
+puts "This code will run."
