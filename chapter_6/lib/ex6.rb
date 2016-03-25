54c579aa188bce2e1e075e259c54573b12144f00
Jarrod Jones <Jaye1200@icloud.com>
2016-03-25 15:47:55 -0600

    Answered chapter 6 questions.
    [113796063]

---
+++ chapter_6/lib/ex6.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+# Assigning the value 10 to the variable types_of_people
+types_of_people = 10
+
+# Creating a string with the value of the variable types_of_people.
+x = "There are #{types_of_people} types of people."
+# Assigning the string value "binary" to the variable binary
+binary = "binary"
+# Assigning the string value "don't" to the variable do_not
+do_not = "don't"
+# Assigning the string value  "Those who know...and those who...with the values of binary and do_not"
+y = "Those who know #{binary} and those who #{do_not}."
+
+# Creating an output with the variables x & y
+puts x
+puts y
+
+# Outputting the value of the variable x into the string "I said"
+puts "I said: #{x}."
+
+# Outputting the value of the variable y into the string "I also said"
+puts "I also said: '#{y}'."
+
+# Assigning the string "false" to the variable hilarious
+hilarious = false
+
+# Creating a variable that contains a string as well a value for the variable "hilarious" within that string.
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# Outputs the value of the variable "joke_evaluation"
+puts joke_evaluation
+
+# Assigning the string "This is the left side of..." to the variable w
+w = "This is the left side of..."
+
+# Assigning the string "a string with a right side." to the variable e
+e = "a string with a right side."
+
+# Adding the values of the variables w and e to create one string.
+puts w + e
