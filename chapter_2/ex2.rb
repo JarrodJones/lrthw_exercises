ee7c0e138960b08484c4d1944309ee2e3c8e1b3e
Jarrod Jones <Jaye1200@icloud.com>
2016-03-30 23:11:33 -0600

    Recommited with pivotal tracker story
    [113796047]

--- chapter_2/ex2.rb
+++ chapter_2/ex2.rb
@@ -8,3 +8,5 @@ puts "I could have code like this." # Comments help by allowing the writer to us
 #Which means that this code will not run but:
 
 puts "This code will run."
+
+# Changes so I can recommit
